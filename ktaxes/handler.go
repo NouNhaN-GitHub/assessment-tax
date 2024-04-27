388d4ddc659a8e34c8755d7d3bb452b51e30e16a
NouNhaN <superhnan@gmail.com>
2024-04-28 00:08:23 +0700

    calculate my tax with tax level detail

--- ktaxes/handler.go
+++ ktaxes/handler.go
@@ -35,8 +35,9 @@ func (h *Handler) TaxCalculationsHandler(c echo.Context) error {
 	if err := c.Bind(&taxRequest); err != nil {
 		return echo.NewHTTPError(http.StatusBadRequest, "bad request body", err.Error())
 	}
-	tax := taxCalculate(taxRequest.TotalIncome, taxRequest.Wht, taxRequest.Allowances)
+	tax, taxLevels := taxCalculate(taxRequest.TotalIncome, taxRequest.Wht, taxRequest.Allowances)
 	res := TaxResponse{}
+	res.TaxLevels = taxLevels
 	res.Tax = tax
 	if tax < 0 {
 		res.Tax = 0
@@ -45,7 +46,7 @@ func (h *Handler) TaxCalculationsHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 }
 
-func taxCalculate(totalIncome float64, wht float64, allowances []Allowance) float64 {
+func taxCalculate(totalIncome float64, wht float64, allowances []Allowance) (float64, []TaxLevel) {
 	netIncome := totalIncome - 60000
 
 	amountDonation := 0.0
@@ -60,6 +61,14 @@ func taxCalculate(totalIncome float64, wht float64, allowances []Allowance) floa
 	}
 	netIncome = netIncome - amountDonation
 
+	displayTaxLevels := []TaxLevel{
+		{"0-150,000", 0.0},
+		{"150,001-500,000", 0.0},
+		{"500,001-1,000,000", 0.0},
+		{"1,000,001-2,000,000", 0.0},
+		{"2,000,001 ขึ้นไป", 0.0},
+	}
+
 	taxLevels := []struct {
 		incomeDiff float64
 		incomeTax  float64
@@ -72,21 +81,23 @@ func taxCalculate(totalIncome float64, wht float64, allowances []Allowance) floa
 	}
 
 	if netIncome <= 150000 {
-		return 0
+		return 0, displayTaxLevels
 	}
 
 	tax := 0.0
-	for _, taxLevel := range taxLevels {
+	for idx, taxLevel := range taxLevels {
 		if netIncome > taxLevel.incomeDiff && taxLevel.incomeDiff != -1 {
 			tax += taxLevel.incomeDiff * taxLevel.incomeTax
 			netIncome -= taxLevel.incomeDiff
+			displayTaxLevels[idx].Tax = taxLevel.incomeDiff * taxLevel.incomeTax
 			continue
 		}
 		tax += netIncome * taxLevel.incomeTax
+		displayTaxLevels[idx].Tax = netIncome * taxLevel.incomeTax
 		netIncome = 0
 	}
 
 	tax = tax - wht
 
-	return tax
+	return tax, displayTaxLevels
 }
